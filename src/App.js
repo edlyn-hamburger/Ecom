2e5d916aa5893c26f732e37b2174a7f87722e06f
Edlyn Pacheco <edlyn@Edlyns-MacBook-Pro.local>
2024-03-29 20:47:59 -0700

    footer & header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Header from "./components/Header"
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Hi Andrew!
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
